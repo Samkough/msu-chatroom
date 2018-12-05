770bddacf8b89fb4cb93929475083c4e1d50414f
Sammy Samkough <sammysamkough@gmail.com>
2018-12-05 08:47:02 -0500

    Almost have console chat ready

--- python/ChatRoomClient.py
+++ python/ChatRoomClient.py
@@ -2,22 +2,31 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-host = input('Host: ')
-port = input('Port: ')
+def receive():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode("utf8")
+            print(message)
+        except OSError:
+            break
 
-address = (host, port)
+def send(event = None):
+    message = input("")
+    print(message)
+    client_socket.send(bytes(message,"utf8"))
+    if message == "[exit]":
+        client_socket.close()
+        
+def quitting():
+    message = "exit"
+    send()
+
+host = input('Enter host: ')
+port = input('Enter port: ')
+address = (host, int(port))
 
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(address)
 
 receive_thread = Thread(target = receive)
 receive_thread.start()
-
-def receive():
-    # receiving message from server
-
-def send():
-    # send message to server
-
-def quitting():
-    # quitting
--- python/ChatRoomServer.py
+++ python/ChatRoomServer.py
@@ -13,21 +13,34 @@ server = socket(AF_INET, SOCK_STREAM)
 server.bind(address)
 
 def accept():
-    # waits for incoming connections
-    # once it gets one, logs the connection and sends client message
-    # stores client in list
-    # start to handle thread for client in handle()
+    while True:
+        client, client_addr = server.accept()
+        print(str(client_addr) + " has connected.")
+        client.send(bytes("Welcome to the MSU Chatroom!", "utf8"))
+        client_addresses[client] = client_addr
+        Thread(target = handle, args = (client,)).start()
 
 def handle(client):
-    # save client name
-    # broadcast to all clients that new client entered
-    # receive messages from client (if quits, it quits)
-    # if quits, close the connecction with client
-    # delete entry of client in list
-    # broadcast to all clients that client left the chat
+    name = client.recv(1024).decode("utf8")
+    client.send(bytes("Welcome" + str(name) + "! To exit, type [exit].", "utf8"))
+    bc_message = str(name) + "joined the chat!"
+    broadcast(bytes(bc_message, "utf8"))
 
-def broadcast(msg, client_name=""):  # prefix is for name identification.
-    # sends message to all clients
+    clients[client] = name
+    while True:
+        msg = client.recv(1024)
+        if msg != bytes("[exit]", "utf8"):
+            broadcast(msg, str(name) + ": ")
+        else:
+            client.send(bytes("[exit]", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes(str(name) + " has left the chat.", "utf8"))
+            break
+
+def broadcast(msg, client_name=""):
+    for sock in clients:
+        sock.send(bytes(client_name, "utf8") + msg)
 
 if __name__ == "__main__":
     server.listen(5)
